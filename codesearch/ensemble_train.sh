f98dcf978a8b990dfed79052d77bcd3931a05532
Xzh0u <littleland.sxz@gmail.com>
2021-04-21 23:19:35 -0700

    feat: update experiments

--- codesearch/ensemble_train.sh
+++ codesearch/ensemble_train.sh
@@ -134,6 +134,8 @@ python ensemble_train.py \
 --per_gpu_eval_batch_size 128 \
 --learning_rate 1e-5 \
 --num_train_epochs 12 \
+--pred_modelA_dir ./models_seed1_struct/java/checkpoint-best/ \
+--pred_modelB_dir ./models_seed1_var/java/checkpoint-best/ \
 --seed 1 \
 --test_file batch_${idx}.txt \
 --pred_model_dir ./models_ensemble2_seed1/$lang/checkpoint-best/ \
@@ -150,6 +152,8 @@ python ensemble_train.py \
 --per_gpu_eval_batch_size 64 \
 --learning_rate 1e-5 \
 --num_train_epochs 12 \
+--pred_modelA_dir ./models_seed3_struct/java/checkpoint-best/ \
+--pred_modelB_dir ./models_seed3_var/java/checkpoint-best/ \
 --test_file batch_${idx}.txt \
 --pred_model_dir ./models_ensemble2_seed3/$lang/checkpoint-best/ \
 --test_result_dir ./results_ensemble2_seed3/$lang/${idx}_batch_result.txt &&
@@ -163,6 +167,8 @@ python ensemble_train.py \
 --max_seq_length 200 \
 --per_gpu_train_batch_size 64 \
 --per_gpu_eval_batch_size 64 \
+--pred_modelA_dir ./models_seed5_struct/java/checkpoint-best/ \
+--pred_modelB_dir ./models_seed5_var/java/checkpoint-best/ \
 --learning_rate 1e-5 \
 --num_train_epochs 12 \
 --test_file batch_${idx}.txt \
@@ -176,6 +182,8 @@ python ensemble_train.py \
 --output_dir ../data/codesearch/test/$lang \
 --data_dir ../data/codesearch/test/$lang \
 --max_seq_length 200 \
+--pred_modelA_dir ./models_seed7_struct/java/checkpoint-best/ \
+--pred_modelB_dir ./models_seed7_var/java/checkpoint-best/ \
 --per_gpu_train_batch_size 64 \
 --per_gpu_eval_batch_size 64 \
 --learning_rate 1e-5 \
@@ -194,6 +202,8 @@ python ensemble_train.py \
 --per_gpu_train_batch_size 64 \
 --per_gpu_eval_batch_size 64 \
 --learning_rate 1e-5 \
+--pred_modelA_dir ./models_seed11_struct/java/checkpoint-best/ \
+--pred_modelB_dir ./models_seed11_var/java/checkpoint-best/ \
 --num_train_epochs 12 \
 --test_file batch_${idx}.txt \
 --pred_model_dir ./models_ensemble2_seed11/$lang/checkpoint-best/ \
